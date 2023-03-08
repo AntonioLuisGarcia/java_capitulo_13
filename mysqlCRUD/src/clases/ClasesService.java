a397a1f502860e8448892cca4933a18cac70c1f6
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-03-08 12:41:07 +0100

    metodo requestAll

---
+++ mysqlCRUD/src/clases/ClasesService.java
@@ -0,0 +1,39 @@
+package clases;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ClasesService {
+    
+    Connection connect;
+
+    public ClasesService(Connection connect){
+        this.connect = connect;
+    }
+
+    public ArrayList<Clase> requestAll()throws SQLException{
+        ArrayList<Clase> listaClases= new ArrayList<Clase>();
+        Statement statement = this.connect.createStatement();
+        String sql = "SELECT ClaseId, ClaseNombre, ClaseProfesor FROM clase";
+        ResultSet resultados = statement.executeQuery(sql);
+
+        while(resultados.next()){
+            int claseId = resultados.getInt("ClaseId");
+            String claseNombre = resultados.getString("ClaseNombre"); 
+            String claseProfesor = resultados.getString("ClaseProfesor");
+            listaClases.add(new Clase(claseId,claseNombre,claseProfesor)); 
+        }
+        
+        statement.close();
+        return listaClases;
+    }
+
+    public Clase requestById(int id){
+        Clase clase = new Clase();
+
+        return clase;
+    }
+}
