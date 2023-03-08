396deadf80eb793e5c24140f2e408de7f222f77b
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-03-08 12:10:40 +0100

    Nueva clase de clase

---
+++ mysqlCRUD/src/clases/Clase.java
@@ -0,0 +1,49 @@
+package clases;
+
+public class Clase {
+    
+    long id;
+    String nombre;
+    String profesor;
+
+    public Clase(){
+        this.id = 0;
+        this.nombre = "";
+        this.profesor = "";
+    }
+
+    public Clase(int id,String nombre,String profesor){
+        this.id = id;
+        this.nombre = nombre;
+        this.profesor = profesor;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    @Override
+    public String toString(){
+        return "ID: " + this.id + ", Clase: " + this.nombre + " ,Profesor: " + this.profesor;
+    }
+}
